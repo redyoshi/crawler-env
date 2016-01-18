e4a47cc4ed2bc0eedfa2df1789905b0025177950
redyoshi <707722543@qq.com>
2016-01-18 11:30:51 +0800

    Update crawler-env.sh

--- crawler-env.sh
+++ crawler-env.sh
@@ -5,6 +5,7 @@ python get-pip.py
 apt-get install -y gcc python-dev libxml2 libxml2-dev libxslt1-dev python-lxml firefox redis-server xvfb xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps  imagemagick python-imaging libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
 
 pip install scrapy selenium redis xvfbwrapper
+rm get-pip.py
 cd /etc/init.d
 wget https://raw.githubusercontent.com/redyoshi/crawler-env/master/xvfb
 chmod +x xvfb
