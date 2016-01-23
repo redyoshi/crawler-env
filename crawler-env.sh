ccfed72d04e3afc47f1e4d807cc353520e433226
redyoshi <707722543@qq.com>
2016-01-24 05:54:23 +0800

    Update crawler-env.sh

--- crawler-env.sh
+++ crawler-env.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 apt-get update
-wget https://raw.githubusercontent.com/pypa/pip/master/contrib/get-pip.py
+wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 apt-get install -y gcc python-dev libxml2 libxml2-dev libxslt1-dev python-lxml firefox redis-server xvfb xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps  imagemagick python-imaging libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
 
